3f442912e9239ecec98c25252a5823f11ce35a10
Bradley <b.malasch@hotmail.com>
2026-03-26 13:02:10 +0100

    Basis opzet voor GamesController

---
+++ LoveMatchDemo/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoveMatchDemo.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
