25d2ed3c7ddeadeb917628a6af77b61f518c08d8
Bradley <b.malasch@hotmail.com>
2026-03-27 10:38:15 +0100

    DataContext aangemaakt

---
+++ LoveMatchDemo/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LoveMatchDemo.Models;
+
+namespace LoveMatchDemo.Data
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+    }
+}
